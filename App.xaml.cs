32c49e2e7aaf87ed158729510c4e8ebc8bf6a15d
Raluca <ralucaburduhos@gmail.com>
2023-11-27 19:44:52 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace Burduhos_Raluca_Lab7;
+﻿using System;
+using Burduhos_Raluca_Lab7.Data;
+using System.IO;
+
+namespace Burduhos_Raluca_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
